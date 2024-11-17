51b994563398f1fba47ebeeda551f687cb1333bd
oryboot <alexi.vernt@gmail.com>
2024-11-17 20:44:07 +0100

    v0.0.1

--- dependences/__init__.py
+++ dependences/__init__.py
@@ -1,5 +1,5 @@
-from .bird import Bird
-from .picoo import Picoo
-from .combat_mecanic import combat, combat_multiple
-from .utils import sort_speed, verif_int
-from .game_logic import jeu
+from bird import Bird
+from picoo import Picoo
+from combat_mecanic import combat, combat_multiple
+from utils import sort_speed, verif_int
+from game_logic import jeu
